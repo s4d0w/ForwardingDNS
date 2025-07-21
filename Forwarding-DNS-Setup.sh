1c7b57fc1d566fd584d92081f3dd1716dc6393ee
jang4sc <jang4sc@gmail.com>
2025-07-21 19:02:16 +0900

    3rd commit

---
+++ Forwarding-DNS-Setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#########################################
+# chmod +x Forwarding-DNS-Setup.sh
+# ./Forwarding-DNS-Setup.sh
+#########################################
+
+set -e
+
+yum -q -y bind bind-utils
+
+cp -p named.conf /etc/named.conf
+cp -p named.rfc1912.zones /etc/named.rfc1912.zones
+
+cp -p example.zone /var/named/example.zone
+cp -p example.rev /var/named/example.rev
+cp -p test.zone /var/named/test.zone
+
+nmcli connection modify eth0 \
+  ipv4.dns 192.168.10.20 \
+  +ipv4.dns 8.8.8.8 \
+  ipv4.dns-search example.com
+nmcli connection up eth0
+
