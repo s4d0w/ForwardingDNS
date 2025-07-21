0a1be3134abc36a6282d9d9d5b94d05735e042e9
jang4sc <jang4sc@gmail.com>
2025-07-21 19:04:26 +0900

    3rd commit

--- Forwarding-DNS-Setup.sh
+++ Forwarding-DNS-Setup.sh
@@ -15,6 +15,12 @@ cp -p example.zone /var/named/example.zone
 cp -p example.rev /var/named/example.rev
 cp -p test.zone /var/named/test.zone
 
+systemctl restart named
+systemctl enable named
+
+firwall-cmd --permanent --add-service=dns
+fireall-cmd --reload
+
 nmcli connection modify eth0 \
   ipv4.dns 192.168.10.20 \
   +ipv4.dns 8.8.8.8 \
